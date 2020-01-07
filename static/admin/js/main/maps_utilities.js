3f0f5b8da5b54ac39b2c300d2c87263c4593737d
lu.baszak13@gmail.com <Gre2poliS>
2020-01-07 23:42:22 +0100

    Updated and created static files.

---
+++ static/admin/js/main/maps_utilities.js
@@ -0,0 +1,64 @@
+    document.addEventListener('DOMContentLoaded', function(){
+        if (document.querySelectorAll('#map').length >0)
+        {
+            if(document.querySelector('html').lang) {
+                lang = document.querySelector('html').lang
+            }
+            else
+                lang ='en';
+
+            var js_file = document.createElement('script');
+            js_file.type = 'text/javascript';
+            js_file.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCxn0UQot5aqIKu3TEoYfObTWNTlyemRwE&callback=initMap&signed_in=true&language=' + lang;
+            document.getElementsByTagName('head')[0].appendChild(js_file);
+        }
+    });
+
+    var map;
+
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+        });
+
+       fetch('/stations/', {
+           method: 'get',
+           credentials: 'same-origin',
+           headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+            },
+       })
+           .then(function(response){
+               return response.json()})
+           .then(plotMarkers);
+
+
+    }
+    var markers;
+    var bounds;
+
+    function plotMarkers(m) {
+        console.log(m)
+        markers = [];
+        bounds = new google.maps.LatLngBounds();
+
+        m.forEach(function(marker){
+            var position = new google.maps.LatLng(marker.latitude, marker.longitude);
+
+            markers.push(
+                new google.maps.Marker({
+                    position: position,
+                    map: map,
+                    animation: google.maps.Animation.DROP
+
+                })
+            );
+
+            bounds.extend(position)
+        });
+
+        map.fitBounds(bounds)
+    }
---
+++ static/utility_functions.js
@@ -0,0 +1,81 @@
+ function parseMillisecondsIntoTime(milliseconds){
+                          //Get hours from milliseconds
+                          var hours = milliseconds / (1000*60*60);
+                          var absoluteHours = Math.floor(hours);
+                          var h = absoluteHours > 9 ? absoluteHours : '0' + absoluteHours;
+
+                          //Get remainder from hours and convert to minutes
+                          var minutes = (hours - absoluteHours) * 60;
+                          var absoluteMinutes = Math.floor(minutes);
+                          var m = absoluteMinutes > 9 ? absoluteMinutes : '0' +  absoluteMinutes;
+
+                          //Get remainder from minutes and convert to seconds
+                          var seconds = (minutes - absoluteMinutes) * 60;
+                          var absoluteSeconds = Math.floor(seconds);
+                          var s = absoluteSeconds > 9 ? absoluteSeconds : '0' + absoluteSeconds;
+                          console.log(hours)
+
+                          return h + ':' + m + ':' + s;
+                          }
+
+function getTrTag(object) {
+
+    var tr = document.createElement('tr');
+
+    start_date = new Date(object.start);
+    end_date = new Date(object.end);
+
+    start_date_formatted = start_date.getDay() + "." + appendLeadingZeroes(start_date.getMonth()+1) + "." + start_date.getFullYear() + ' ' + start_date.getHours() + '.' + start_date.getMinutes() + '.' + start_date.getSeconds();
+    end_date_formatted = end_date.getDay() + "." + appendLeadingZeroes(end_date.getMonth()+1) + "." + end_date.getFullYear() + ' ' + end_date.getHours() + '.' + end_date.getMinutes() + '.' + end_date.getSeconds();
+
+    duration = end_date - start_date;
+    duration_formatted = parseMillisecondsIntoTime(duration);
+
+
+    if(object.price != null) {
+        tr.innerHTML = '<td>' + '<i class="fas fa-info-circle"></i>' +  '</td>' +
+                    '<td>' + start_date_formatted + '</td>' +
+
+                    '<td>' + end_date_formatted + '</td>' +
+                    '<td>' + duration_formatted + '</td>' +
+                    '<td>' + object.price + "zl" + '</td>';
+    }
+
+    else {
+        tr.innerHTML = '<td>' + 'lol' +  '</td>' +
+         '<td>' + start_date_formatted + '</td>' +
+          '<td>' + " - " + '</td>' +
+          '<td>' + " - " + '</td>' +
+          '<td>' + " - " + '</td>';
+
+    }
+
+    tr.onclick = function(evt) {
+        popupWindow = window.open(
+            'details/'+object.id,'popUpWindow','height=500 ,width=500,left=10,top=10,' +
+            'scrollbars=no,toolbar=no,menubar=no,location=no,' +
+            'directories=no,status=yes' +
+            'details={{object}}')
+    };
+
+
+    tr.onmouseover = function(evt) {
+        tr.style.backgroundColor = '#78909c'
+    };
+    tr.onmouseleave = function(evt) {
+        tr.style.backgroundColor = '#546e7a'
+    };
+
+
+
+    return tr;
+}
+
+
+function appendLeadingZeroes(n){
+      if(n <= 9){
+        return "0" + n;
+      }
+      return n
+        }
+
